c6a8ab3393942520b511bdfa236fc1820d441b42
Saron Medeiros Philippi <saron.ph@gmail.com>
2022-02-17 19:06:31 -0300

    added header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
-import styles from '../styles/home.module.scss'
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <h1 className={styles.title}>Hello</h1>
+  return (  
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>  
+      <h1>      
+        Hello
+      </h1>
+    </>
   )
 }
